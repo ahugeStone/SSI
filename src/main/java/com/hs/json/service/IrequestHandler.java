916d62e286fd42ca005dd6f28392abffb371d663
Shi Huang <742969055@qq.com>
2015-05-03 23:24:02 +0800

    Revert "整合SSI的代码"
    This reverts commit 68dc31f66a05e56dcfc195c46a21f13e070beb11.

--- src/main/java/com/hs/json/service/IrequestHandler.java
+++
@@ -1,9 +0,0 @@
-package com.hs.json.service;
-
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface IrequestHandler {
-	public Map<String, String> result(HttpServletRequest req);
-}
--- target/SSI/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>This is jsp!</h2>
-</body>
-</html>
